fdf8ce1f755b9b61caa0d9efeebec860a7a6e183
BANDELSOM <133780894+BANDELSOM@users.noreply.github.com>
2024-04-20 21:57:13 +0100

    portfolio João bandeira

---
+++ javaScript/js.js
@@ -0,0 +1,12 @@
+
+let numeros=document.getElementById('numero')
+
+function abrir(){
+    
+    if (numeros.style.display==="none") {
+    numeros.style.display="block"
+}else{
+    numeros.style.display="none"
+}
+  
+}
